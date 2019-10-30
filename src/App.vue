98f393dcc8ca3a39ffaa41f6e3913336aeaf3368
Hardtodefine <56499565+Hardtodefine@users.noreply.github.com>
2019-10-30 14:29:50 +0800

    Day1

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="wrapper">
+      <div>第{{n}}手</div>
+      <div class="row">
+        <Cell @click="onClickCell(0, $event)" :n="n"/>
+        <Cell @click="onClickCell(1, $event)" :n="n"/>
+        <Cell @click="onClickCell(2, $event)" :n="n"/>
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(3, $event)" :n="n"/>
+        <Cell @click="onClickCell(4, $event)" :n="n"/>
+        <Cell @click="onClickCell(5, $event)" :n="n"/>
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(6, $event)" :n="n"/>
+        <Cell v-on:click="onClickCell(7, $event)" v-bind:n="n"/>
+        <Cell v-on:click="onClickCell(8, $event)" v-bind:n="n"/>
+      </div>
+      <div>结果:{{result === null ? `胜负未分` : `胜方为${result}`}}</div>
+    </div>
+</template>
+<script>
+import Cell from './Cell'
+  export default{
+    components:{Cell},
+    data(){
+      return {n:0,map:[
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+      ],result:''};
+    },
+    methods:{
+      onClickCell(i,text){
+        console.log(`${i}号被点击`);
+        // console.log(this.vm);//用不了
+        // this.map[2][2] = i;
+        // console.log(this.map);
+        // i
+        // 0  map[0][0]
+        // 3  map[1][0]
+        // 4  map[1][1]
+        this.map[Math.floor(i/3)][i%3] = text
+        this.n = this.n+1;
+        this.tell();
+      },
+      tell(){
+        const map = this.map;
+      // let result = false
+      if(map[0][0] !== null && map[0][0] === map [1][1] && map[1][1] === map [2][2]){
+            this.result = map[0][0];
+            // Object.freeze(this.result);
+            // const this.result = map[2][2];
+            return
+          }
+      if(map[2][0] !== null && map[0][2] === map [1][1] && map[1][1] === map [2][0]){
+            this.result = map[0][2];
+            return
+          }
+        for(let i=0;i<2;i++){
+          if(map[i][0] !== null && map[i][0] === map [i][1] && map[i][1] === map[i][2]){
+            this.result = map[i][0];
+            return
+            }
+        }
+        for(let j=0;j<2;j++){
+          if(map[0][j] !== null && map[0][j] === map [1][j] && map[1][j] === map [2][j]){
+            this.result = map[0][j];
+            return
+          }
+        }
+      }
+    }
+  }
+</script>
+<style>
+.wrapper{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -150px;
+  margin-left: -150px; 
+}
+.row{
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ src/Cell.vue
@@ -0,0 +1,40 @@
+<template class="cell">
+  <div>
+    <div class="cell" v-on:click="onClickSelf">
+      <template v-if="a">{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    props:["n"],
+    data(){
+      return{a:false,text:null}
+    },
+    // computed:{
+    //   text(){return this.n % 2 == 0 ? 'x' : 'o';}
+    // },
+    methods:{
+      onClickSelf(){
+        if(this.text != null){
+          return;
+        };
+        this.a = true;
+        this.text = this.n % 2 == 0 ? 'x' : 'o';
+        this.$emit('click',this.text);
+      }
+    }
+  };
+</script>
+<style>
+.cell{
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 80px;
+}
+</style>
